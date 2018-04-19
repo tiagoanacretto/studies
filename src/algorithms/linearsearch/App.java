b0d50c15344baadd970aa410a801c5b74ce9f939
tiagoanacretto <tiago.anacretto@gmail.com>
2018-04-19 20:43:28 -0300

    Class 18 - Linear Search

--- src/algorithms/linearsearch/App.java
+++ src/algorithms/linearsearch/App.java
@@ -2,4 +2,18 @@ package algorithms.linearsearch;
 
 public class App {
 
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int linearSearch(int[] a, int x) {
+		int answer = -1;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == x) {
+				answer = i;
+				break;
+			}
+		}
+		return answer;
+	}
 }
