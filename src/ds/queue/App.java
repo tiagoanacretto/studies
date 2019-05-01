8ac1a35f41b4d03930966bc332ecc9ffeb7b3ca9
tiagoanacretto <tiago.anacretto@gmail.com>
2019-04-30 23:29:35 -0300

    Some comments in 11th class

--- src/ds/queue/App.java
+++ src/ds/queue/App.java
@@ -7,10 +7,18 @@ public class App {
 		myQueue.insert(100);
 		myQueue.insert(1000);
 		myQueue.insert(14);
+		myQueue.view();
+		System.out.println(String.format("Removed: %d", myQueue.remove()));
+		myQueue.view();
 		myQueue.insert(12);
 		myQueue.insert(44);
-		myQueue.insert(33);
-		myQueue.insert(8);
+		//myQueue.insert(33);
+		//myQueue.insert(8);
+		myQueue.view();
+		
+		System.out.println(String.format("Removed: %d", myQueue.remove()));
+		myQueue.view();
+		myQueue.insert(90);
 		myQueue.view();
 	}
 
--- src/ds/queue/Queue.java
+++ src/ds/queue/Queue.java
@@ -2,22 +2,22 @@ package ds.queue;
 
 public class Queue {
 
-	private int maxSize;
-	private long[] queueArray;
-	private int front;
-	private int rear;
-	private int nItems;
+	private int maxSize;       // initializes the set number of slots
+	private long[] queueArray; // slots to main the data
+	private int front;         // this will be the index position for the element in the front
+	private int rear;          // this is also going to be the index position for the element at the back of the line
+	private int nItems;        // counter to maintain the number of items in our queue
 	
 	public Queue(int size) { 
 		this.maxSize = size;
 		this.queueArray = new long[size];
-		this.front = 0;
-		this.rear = -1;
-		this.nItems = 0;
+		this.front = 0; // index position of the first slot of the array
+		this.rear = -1; // there is no items in the array yet to be considered as the last item
+		this.nItems = 0; // we don't have elements in the array yet
 	}
 	
 	public void insert(long j) {
-		if (rear == maxSize - 1) { // Circular Queue
+		if (rear == maxSize - 1) { // Circular Queue - this overwrite the first position when the array is full
 			rear = -1;
 		}
 		rear++;
@@ -25,17 +25,17 @@ public class Queue {
 		nItems++;
 	}
 	
-	public long remove() {
+	public long remove() { // remove item from the front of the queue
 		long temp = queueArray[front];
 		front++;
 		if (front == maxSize) {
-			front = 0;
+			front = 0; // we can set front back to the 0th index so that we can utilize the entire array again
 		}
 		nItems--;
 		return temp;
 	}
 	
-	public long peekFront() {
+	public long peekFront() { // just to get the first element
 		return queueArray[front];
 	}
 	
@@ -52,6 +52,6 @@ public class Queue {
 		for (int i = 0; i < queueArray.length; i++) {
 			System.out.print(queueArray[i]+ " ");
 		}
-		System.out.print("]");
+		System.out.print("]\n");
 	}
 }
